c4279c98b3aa828a8fd124533da4d8e6ec4b62c4
Agia <agia@pm.me>
2023-03-15 16:21:18 +0000

    Add skeleton Review component, and CSS resets / variables to main index.css

---
+++ src/components/Review/Review.js
@@ -0,0 +1,28 @@
+// Imports
+import { React, useState } from "react";
+
+
+const Review = () => {
+    // const [bookTitle, getBookTitle] = useState("");
+    // const [author, getAuthor] = useState("");
+
+    return (
+        <div id="review">
+            <h2>Book Review</h2>
+            <div className="book-review">
+                <div className="left-page">
+                    <form>
+
+
+                    </form>
+
+                </div>
+                <div className="right-page">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Review;
