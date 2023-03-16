975fb3edf978385cbf63c21048c5694e9d543e05
Eliyas <eliyas.kapasi@gmail.com>
2023-03-16 15:12:40 +0000

    started the home page

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,27 @@
+//Imports
+import React from "react";
+import "./style.css";
+//<style>
+//  @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,700&display=swap');
+//</style>
+
+//Landing section code
+const Home = () => {
+    return (
+        <div id = "home">
+            <h1>The Reading Room</h1>
+            <welcome>What are you reading?</welcome>
+            <button>Review now</button>
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+//Exports
+export default Home
